8d19ed2d3eb5961476597d8ff2f39e6c7ec32ace
Michael Gerstmann <michael.gerstmann@fitx.de>
2018-08-08 17:10:28 +0200

    document date helper

--- src/helper/date.js
+++ src/helper/date.js
@@ -1,9 +1,18 @@
 import { DateTime } from 'luxon';
 
+/**
+ * Get Now
+ * @returns {DateTime} - ISO 8601
+ */
 function getToday() {
   return DateTime.local();
 }
 
+/**
+ * Get Weeks and Days via End-date from Now
+ * @param {string} date - YYYY-MM-DD
+ * @returns {Duration}
+ */
 function calculateCurrentWeek(date) {
   const end = DateTime.fromISO(DateTime.local().toISODate());
   const start = DateTime.fromISO(date);
@@ -11,6 +20,11 @@ function calculateCurrentWeek(date) {
   return end.diff(start, ['weeks', 'days']);
 }
 
+/**
+ * Get calculated Date of Birth
+ * @param {string} lastDayOfPeriod - YYYY-MM-DD
+ * @returns {string} - DateTime ISO 8601
+ */
 function calculateDateOfBirth(lastDayOfPeriod) {
   const startDate = DateTime.fromISO(lastDayOfPeriod);
   return startDate
@@ -21,6 +35,11 @@ function calculateDateOfBirth(lastDayOfPeriod) {
     .toISODate();
 }
 
+/**
+ * Calculate last day of Period
+ * @param {string} dateOfBirth - YYYY-MM-DD
+ * @returns {string} - DateTime ISO 8601
+ */
 function calculateLastDayOfPeriod(dateOfBirth) {
   const endDate = DateTime.fromISO(dateOfBirth);
   return endDate
